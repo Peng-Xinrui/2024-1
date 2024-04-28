75c9750b3d43944a64b6fbb3af9940214b08ccde
PXR <84264706+Peng-Xinrui@users.noreply.github.com>
2024-04-29 01:18:21 +0800

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def calculate_accuracy_and_distribution(file1_path, file2_path):
     # 读取文件
-    df1 = pd.read_csv(file1_path, header=None)
+    df1 = pd.read_csv(file1_path)
     df2 = pd.read_csv(file2_path)
 
     # 设定列名
@@ -12,7 +13,7 @@ def calculate_accuracy_and_distribution(file1_path, file2_path):
 
     # 检查和对齐位置
     if not df1['Position'].equals(df2['Position']):
-        raise ValueError("位置信息不匹配。请检查两个文件的位置列。")
+        raise ValueError("位置信息不匹配。请检查两个文件的位置列。"+str(file1_path))
 
     # 计算每个样本的准确率和状态分布
     accuracies = {}
@@ -24,6 +25,7 @@ def calculate_accuracy_and_distribution(file1_path, file2_path):
         distributions[col] = (dist1, dist2)
 
     return accuracies, distributions
+
 
 def plot_and_save_pie_charts(distributions, chromosome):
     for sample, dist in distributions.items():
@@ -41,12 +43,20 @@ def plot_and_save_pie_charts(distributions, chromosome):
         plt.savefig(f'E:/毕业设计/ancestryhmm/chrome_{chromosome}_{sample}_pie_chart.svg')
         plt.close()
 
+
+# 创建一个空DataFrame来收集所有准确率数据
+accuracy_df = pd.DataFrame()
+
 # 主循环
-for ind in [1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]:
+for ind in [1, 2, 3, 4, 5, 6, 8,  10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]:
     file1_path = f'E:/毕业设计/ancestryhmm/chrome_{ind}_ans.csv'
     file2_path = f'E:/毕业设计/ancestryhmm/mlp_results_{ind}.csv'
     accuracies, distributions = calculate_accuracy_and_distribution(file1_path, file2_path)
-    print(f'Chromosome {ind}')
-    for sample, accuracy in accuracies.items():
-        print(f"Accuracy {sample}: {accuracy:.2%}")
+
+    # 将当前染色体的准确率添加到DataFrame
+    accuracy_df = pd.concat([accuracy_df, pd.DataFrame(accuracies, index=[f'Chromosome {ind}'])])
+
     plot_and_save_pie_charts(distributions, ind)
+
+# 保存准确率数据到CSV文件
+accuracy_df.to_csv('E:/毕业设计/ancestryhmm/accuracy_chrome_22.csv')
